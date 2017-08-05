a2c3a42cffcae7ef4827208a96849aef3bf9f0ad
Pasindu <pasindumuth@gmail.com>
2017-08-04 23:20:35 -0700

    more compact ribbon height and more colors

--- public/Canvas.js
+++ public/Canvas.js
@@ -1,7 +1,8 @@
 "use strict";
 
 // UI constants
-let RIBBON_HEIGHT = 10,
+// we refer to a single ribbon as a "timeline", as well as the set of all ribbons
+let RIBBON_HEIGHT = 6,
     THREAD_GAP = 20,
     TIMELINE_TOP_PADDING = 15,
     TIMELINE_BOTTOM_PADDING = 10,
@@ -31,11 +32,14 @@ let NAME_SIDE_BAR_LEFT_OFFSET = TIMELINE_BAR_LEFT_OVERFLOW,
     NAME_FONT_SIZE = 15,
     NAME_FONT_COLOR = "#000000";
 
-let ALL_COLORS = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", 
-                  "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99",
-                  "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262",
-                  "#5574a6", "#3b3eac"];
 
+// Extended color set. use the commented out colors if they are more suitable.
+let ALL_COLORS = ["#969664","#fa0064","#9632c8","#3264fa","#966464","#32c864","#fa6496","#6432c8","#c864c8","#96c896","#c800c8","#fa3232","#64fa64","#6432fa","#00fafa","#00c8fa","#0032fa","#00fac8","#32c896","#3200fa","#fa9696","#643296","#64fac8","#96fa64","#fa6432","#64c800","#6400fa","#96fa00","#9600c8","#6496c8","#0064fa","#32c8fa","#6464c8","#32fafa","#00fa64","#fafa96","#fa64fa","#c8fa96","#c8c8fa","#00fa32","#c832fa","#c83264","#c8c832","#fafac8","#64c864","#00fa96","#fac832","#32c832","#fa3296","#00c8c8","#c80064","#fafa32","#96c8c8","#3296c8","#32fa96","#fa0096","#009696","#9632fa","#64fa96","#c8c864","#c86400","#c896fa","#fa32fa","#c8fa64","#329664","#963296","#c89696","#96fafa","#32fa32","#96c8fa","#c89664","#963264","#fa9600","#6400c8","#96fa32","#c86432","#64fafa","#9600fa","#fa3200","#fa9632","#32fac8","#96c800","#c8fa32","#c83296","#c8fafa","#c896c8","#fa6464","#96fac8","#646464","#c800fa","#fac8fa","#32fa64","#fa96fa","#fafa00","#6496fa","#969600","#c83232","#fa64c8","#64c8c8","#fa9664","#96fa96","#0064c8","#64fa00","#64c8fa","#c864fa","#fa32c8","#969696","#3232c8","#fafa64","#329696","#96c832","#00c896","#64c832","#c8fa00","#966432","#960096","#c86464","#fac800","#966496","#969632","#6464fa","#c8fac8","#32c8c8","#fa00c8","#3232fa","#9664c8","#c8c800","#3264c8","#fa96c8","#9696fa","#fa3264","#fac864","#fa00fa","#646496","#64fa32","#0096c8","#649664","#326496","#649632"];
+
+// let ALL_COLORS = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", 
+//                   "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99",
+//                   "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262",
+//                   "#5574a6", "#3b3eac"];
 
 let Canvas = function (timeframePanelsRaw, timelineData, viewportMetaData) {
     this.timelineData = timelineData;
